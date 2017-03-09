5b36c9928480d70ec45da0b676a95b8d445e14d5
salad7 <msalad@uncc.edu>
2017-03-09 02:54:12 -0500

    Added actionbar

--- app/src/main/java/com/example/mohamed/tedradiohour/MainActivity.java
+++ app/src/main/java/com/example/mohamed/tedradiohour/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.mohamed.tedradiohour;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActionBar actionBar;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //actionBar = getSupportActionBar();
+        //actionBar.show();
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
     }//
 }
